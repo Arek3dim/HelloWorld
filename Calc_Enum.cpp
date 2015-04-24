33443266f8901c4aaf637ba9bc5eea067c0c99e0
Arek3dim <Arek.keshishian@gmail.com>
2015-04-24 20:26:41 +0400

    holy shit!

---
+++ Calc_Enum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+enum OpType { Add = '+', Sub = '-', Mul = '*', Dvd = '/'};
+
+int main()
+{
+	OpType op;
+	double a;
+	double b;
+	char CharOp;
+	
+
+	cout << "Enter a: # \n";
+	cin >> a;
+	cout << "Enter operation: \n";
+	cin >> CharOp;
+	cout << "Enter b: # \n";
+	cin >> b;
+
+	op = static_cast<OpType>(CharOp);
+
+	double res;
+	switch (op)
+
+	{
+	case '+':
+		res = a + b;
+		cout << a << " + " << b << " = " << res << endl;
+		break;
+
+	case '-':
+		res = a - b;
+		cout << a << " - " << b << " = " << res << endl;
+		break;
+
+	case '*':
+		res = a * b;
+		cout << a << " * " << b << " = " << res << endl;
+		break;
+
+	case '/':
+		res = a / b;
+		cout << a << " / " << b << " = " << res << endl;
+
+	default:
+		cout << "Invalid operation entered!" << endl;
+
+	}
+}
